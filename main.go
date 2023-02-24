bd120d84a0c02e57843bdb28fe221c4e67254841
x1a0xf <yyzw@live.com>
2023-02-24 23:51:07 +0800

    feat: 修改参数

--- main.go
+++ main.go
@@ -14,33 +14,35 @@ import (
 
 var (
 	flags []cli.Flag
-	t     string
 	n     int
 )
 
 const (
 	RegDetails       = `<details>[\s\S]*?</details>`
 	RegUnorderedList = `(?m)^-\s(.*)`
+	RegMD            = `*.md`
 )
 
 func init() {
 	flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "t, tag",
-			Value:       "ms",
-			Usage:       "tag files",
-			Destination: &t,
-		},
 		cli.IntFlag{
 			Name:        "n, num",
 			Value:       30,
 			Usage:       "num of questions",
 			Destination: &n,
+			Required:    true,
 		},
 		cli.StringSliceFlag{
-			Name:  "d, dirs",
-			Value: &cli.StringSlice{"/Users/lhgtqb7bll/docs/arch", "/Users/lhgtqb7bll/docs/database"},
-			Usage: "dirs of files",
+			Name:     "w, wf",
+			Value:    &cli.StringSlice{},
+			Usage:    "files",
+			Required: true,
+		},
+		cli.StringSliceFlag{
+			Name:     "e, exclude",
+			Value:    &cli.StringSlice{},
+			Usage:    "exclude specified files",
+			Required: false,
 		},
 	}
 }
@@ -48,7 +50,7 @@ func init() {
 func main() {
 	app := cli.NewApp()
 	app.Name = "Weekly-Training"
-	app.Usage = "use to collocate with gh-ac, generate weekly training"
+	app.Usage = "use to collocate with gh-ac, generate weekly training items"
 	app.HideVersion = true
 	app.Flags = flags
 	app.Action = Action
@@ -60,26 +62,39 @@ func main() {
 }
 
 func Action(c *cli.Context) error {
-	dirs := c.StringSlice("dirs")
+	wf := c.StringSlice("wf")
+	ex := c.StringSlice("exclude")
 	num := c.Int("num")
 	res := make([]string, 0)
 
-	for _, dir := range dirs {
-		isDir := gfile.IsDir(dir)
-		if !isDir {
-			return cli.NewExitError("not a dir", 2)
-		}
-		files, err := gfile.ScanDir(dir, "*.md", true)
-		if err != nil {
-			return cli.NewExitError(err.Error(), 2)
-		}
-		for _, file := range files {
-			isFile := gfile.IsFile(file)
+	for _, w := range wf {
+		isDir := gfile.IsDir(w)
+		if isDir {
+			files, err := gfile.ScanDir(w, RegMD, true)
+			if err != nil {
+				return cli.NewExitError(err.Error(), 2)
+			}
+			for _, file := range files {
+				isFile := gfile.IsFile(file)
+				if !isFile {
+					return cli.NewExitError("not a file", 2)
+				}
+				fArr := strings.Split(file, "/")
+				if !garray.NewStrArrayFrom(ex).Contains(fmt.Sprintf("%s/%s", fArr[len(fArr)-2], fArr[len(fArr)-1])) {
+					qs := ExtractQuestion(file)
+					res = append(res, qs...)
+				}
+			}
+		} else {
+			isFile := gfile.IsFile(w)
 			if !isFile {
 				return cli.NewExitError("not a file", 2)
 			}
-			qs := ExtractQuestion(file)
-			res = append(res, qs...)
+			fArr := strings.Split(w, "/")
+			if !garray.NewStrArrayFrom(ex).Contains(fmt.Sprintf("%s/%s", fArr[len(fArr)-2], fArr[len(fArr)-1])) {
+				qs := ExtractQuestion(w)
+				res = append(res, qs...)
+			}
 		}
 	}
 
@@ -113,7 +128,7 @@ func ExtractQuestion(file string) []string {
 // GenerateMD 生成最终的md文档
 func GenerateMD(qs []string) (rt string) {
 	for i := 0; i < len(qs); i++ {
-		rt += "- [ ] " + strings.Replace(qs[i], "- ", "", -1) + "\n"
+		rt += "- [ ] " + strings.Replace(qs[i], "- ", "", -1) + "\n\n"
 	}
 	return
 }
