c81884f8fe20ff9642c38629a7aa16428c140eb5
x1a0xf <yyzw@live.com>
2023-12-30 20:54:43 +0800

    feat: Support multi-yml read data

--- cmd/feed.go
+++ cmd/feed.go
@@ -3,10 +3,15 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 	"time"
-	"unsafe"
+
+	"gopkg.in/yaml.v3"
 
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/extension"
@@ -14,12 +19,15 @@ import (
 	"github.com/yuin/goldmark/renderer/html"
 
 	"github.com/gorilla/feeds"
-	"gopkg.in/yaml.v3"
-
 	"github.com/spf13/cobra"
 )
 
-type Doc struct {
+type qs struct {
+	FileName string
+	Doc      []doc
+}
+
+type doc struct {
 	Docs string   `yaml:"docs"`
 	Qs   []string `yaml:"qs"`
 }
@@ -28,30 +36,95 @@ type Doc struct {
 var feedCmd = &cobra.Command{
 	Use:   "feed",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		filename := args[0]
+		folderName := args[0]
 		outputFile := args[1]
-		yamlData, err := os.ReadFile(filename)
+
+		type fi struct {
+			FilePath string
+			FileName string
+		}
+
+		fileName := make(chan fi)
+
+		_, err := os.Stat(folderName)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 
-		var data []Doc
-		if err := yaml.Unmarshal(yamlData, &data); err != nil {
-			log.Fatal(err)
+		go func() {
+			err = filepath.WalkDir(folderName, func(fp string, d fs.DirEntry, err error) error {
+				if !d.IsDir() && (strings.HasSuffix(fp, "yml") || strings.HasSuffix(fp, "yaml")) {
+					fileName <- fi{FilePath: fp, FileName: d.Name()}
+				}
+				return nil
+			})
+			close(fileName)
+		}()
+
+		// rssFeed := make(chan string)
+		rssFeed := make(chan qs)
+
+		go func(fileName chan fi) {
+			var wg sync.WaitGroup
+			for f := range fileName {
+				wg.Add(1)
+				go func(f fi) {
+					defer wg.Done()
+
+					yamlData, err := os.ReadFile(f.FilePath)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					var data []doc
+					if err := yaml.Unmarshal(yamlData, &data); err != nil {
+						log.Fatal(err)
+					}
+					rssFeed <- qs{FileName: f.FileName, Doc: data}
+					// select {
+					// case rssFeed <- qs{FileName: f.FileName, Doc: data}:
+					// default:
+					// 	// Handle the case when the rssFeed channel is blocked
+					// 	log.Println("rssFeed channel is blocked. Skipping data.")
+					// }
+				}(f)
+			}
+			wg.Wait()
+			close(rssFeed)
+		}(fileName)
+
+		// var res string
+		var resBuilder strings.Builder
+		for rf := range rssFeed {
+			// res += generateHTML(rf)
+			resBuilder.WriteString(generateHTML(rf))
 		}
 
-		rssFeed := generateRSS(data)
-		if err := os.WriteFile(outputFile, []byte(rssFeed), 0o644); err != nil {
+		// for {
+		// 	select {
+		// 	case rf, ok := <-rssFeed:
+		// 		if !ok {
+		// 			// rssFeed channel has been closed
+		// 			rssFeed = nil
+		// 			break
+		// 		}
+		// 		resBuilder.WriteString(generateHTML(rf))
+		// 	default:
+		// 		// Handle the case when the rssFeed channel is blocked
+		// 		log.Println("rssFeed channel is blocked. Exiting the loop.")
+		// 		rssFeed = nil
+		// 	}
+		// 	if rssFeed == nil {
+		// 		break
+		// 	}
+		// }
+
+		rss := generateRSS(resBuilder.String())
+
+		if err := os.WriteFile(outputFile, []byte(rss), 0o644); err != nil {
 			log.Fatal(err)
 		}
-
 		fmt.Println("Successfully wrote RSS feed to file")
 	},
 }
@@ -70,14 +143,14 @@ func init() {
 	// feedCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-func generateRSS(data []Doc) string {
+func generateRSS(res string) string {
 	feed := &feeds.Feed{
 		Title:       "docs",
 		Link:        &feeds.Link{Href: "https://github.com/hxhacking/docs"},
 		Description: "just for fun",
 	}
 
-	res := generateHTML(data)
+	// res := generateHTML(qs)
 	feed.Items = append(feed.Items, &feeds.Item{
 		Title:       "docs",
 		Link:        &feeds.Link{Href: "https://github.com/hxhacking/docs"},
@@ -93,10 +166,10 @@ func generateRSS(data []Doc) string {
 	return rss
 }
 
-func generateHTML(data []Doc) string {
-	questions := ""
-	for _, item := range data {
-		questions += fmt.Sprintf("# %s \n\n", item.Docs)
+func generateHTML(qs qs) string {
+	questions := fmt.Sprintf("\n\n\n# %s \n\n\n", qs.FileName)
+	for _, item := range qs.Doc {
+		questions += fmt.Sprintf("## %s \n\n", item.Docs)
 		for _, question := range item.Qs {
 			questions += fmt.Sprintf("- %s \n", question)
 		}
@@ -134,13 +207,13 @@ func Md2HTML(md string) string {
 			html.WithXHTML(),
 		),
 	)
-	if err := markdown.Convert(StringToBytes(md), &buf); err != nil {
+	if err := markdown.Convert([]byte(md), &buf); err != nil {
 		return ""
 	}
 
 	return buf.String()
 }
 
-func StringToBytes(s string) []byte {
-	return unsafe.Slice(unsafe.StringData(s), len(s))
-}
+// func StringToBytes(s string) []byte {
+// 	return unsafe.Slice(unsafe.StringData(s), len(s))
+// }
